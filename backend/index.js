c12c8bd4c7e232f9c0879debffc1976b4f4ac08d
PriscillaCole <51949337+PriscillaCole@users.noreply.github.com>
2024-06-23 22:03:45 +0300

    serving the index.html page for the root url

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path');
 const bodyParser = require('body-parser');
-const movieRouter = require('./routes');
+const movieRouter = require('./routes'); // Ensure this points to the correct path
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
 const port = 3000;
+
+// Serve static files from the "frontend" directory
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +18,10 @@ app.options('*', cors());
 app.use(cors());
 app.use('/movies', movieRouter);
 
+// Serve the index.html file for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
